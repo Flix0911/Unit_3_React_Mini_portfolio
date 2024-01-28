1f6a9cc00aa7a3ed0c72a7eacb1e69c9cd7e17d7
Erick Valencia <evalencia0911@gmail.com>
2024-01-28 11:27:10 -0700

    style for header, main, and project completed. next is footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 const Header = (props) => {
     //component returned in JSX
-    return <div className="Header">
-        <a href="https://github.com/Flix0911">GitHub</a>
-        <br className="break" />
-        <a href="https://www.linkedin.com/in/erickvalenciadev/">LinkedIn</a>
+    return <div className="header">
+        <h1 className="creator-name">Erick Valencia's Portfolio</h1>
+        <div className="navigation-container">
+            <a href="https://github.com/Flix0911">GitHub</a>
+            <br className="break" />
+            <a href="https://www.linkedin.com/in/erickvalenciadev/">LinkedIn</a>
+        </div>
     </div>
 }
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,15 +8,15 @@ const Projects = (props) => {
             <h1>Country Look-Up</h1>
             <h3>Learn more about Countries you want to visit!</h3>
             <a href="https://github.com/Flix0911/project1_country_look-up?tab=readme-ov-file">GitHub Repo</a>
-            <img src={project1Image} alt="Project 1 Image" />
             <p>This is a project that utilized JavaScript, CSS, HTML, Jquery, and a RestAPI, to show the user information about a country they input.</p>
+            <img src={project1Image} alt="Project 1 Image" />
         </div>
         <div className="project2">
-        <h1>Record your Records</h1>
+            <h1>Record your Records</h1>
             <h3>Store your physical music collection, online!</h3>
             <a href="https://github.com/Flix0911/project2">GitHub Repo</a>
-            <img src={project2Image} alt="Project 2 Image" />
             <p>This was a full-stack web application that utilizied JavaScript, HTML, and CSS. I used MongoDB for my database and my utilized dependencies are: connect-mango, dotenv, ejs, express, method-override, mongoose, and morgan</p>
+            <img src={project2Image} alt="Project 2 Image" />
         </div>
     </div>
 }
