0beec5bd5f52ab196dbfd2425bc251439750b2a3
Erick Valencia <evalencia0911@gmail.com>
2024-01-29 19:32:02 -0700

    footer! It looks hilarious

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,31 @@
+import profileImage from '../images/profile.jpeg'
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+
+
 const Footer = (props) => {
     //component returned in JSX
-    return <div className="Footer">This is a Footer</div>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+
+    return <div className="card">
+        <div className="profile-sidebar">
+            <img className="profile-image" src={profileImage} alt="Profile Image"/>
+                <ul className='social-list'>
+                    <li className='social-item'><a className='social-link' href="https://www.linkedin.com/in/erickvalenciadev/">LinkedIn</a></li>
+
+                    <li className='social-item'><a className='social-link' href="https://github.com/Flix0911">GitHub</a></li>
+
+                </ul>
+            </div>
+            <div className='profile-main'>
+                <h4 className='profile-name'>Erick Valencia</h4>
+                <p className='profile-position'>Software Engineer</p>
+                <p className='profile body'>
+                    Lorem
+                </p>
+            
+            </div>
+        </div>
+
 }
 
 export default Footer
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,10 @@ const Header = (props) => {
         <h1 className="creator-name">Erick Valencia's Portfolio</h1>
         <div className="navigation-container">
             <a href="https://github.com/Flix0911">GitHub</a>
-            <br className="break" />
             <a href="https://www.linkedin.com/in/erickvalenciadev/">LinkedIn</a>
+            <a href="google.com">Resume</a>
+            <a href="google.com">About</a>
+            <a href="google.com">Contact</a>
         </div>
     </div>
 }
